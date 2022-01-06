6cbe2776d499d71fc680a01efb5683dd939692c5
FernandoCelmer <email@fernanocelmer.com>
2022-01-05 21:40:02 -0300

    UPY-#15 - Create first model

--- service/admin.py
+++ service/admin.py
@@ -1,1 +1,17 @@
+# coding=utf-8
+
+"""
+Module Admin
+"""
+
+
 from django.contrib import admin
+from app.utils import get_field_list
+
+from service.models import WorkStation
+
+class WorkStationAdmin(admin.ModelAdmin):
+    list_display = get_field_list(WorkStation)
+
+
+admin.site.register(WorkStation, WorkStationAdmin)
--- service/models.py
+++ service/models.py
@@ -1,1 +1,28 @@
+# coding=utf-8
+
+"""
+Model Config
+"""
+
 from django.db import models
+
+class WorkStation(models.Model):
+    """
+    Model WorkStation
+    """
+    key = models.CharField(max_length=25, blank=False, null=False)
+    description = models.CharField(max_length=100, blank=False, null=False)
+    ip_address = models.CharField(max_length=15, blank=False, null=False)
+    mac_address = models.CharField(max_length=50, blank=False, null=False)
+    user_name = models.CharField(max_length=25, blank=False, null=False)
+    host = models.CharField(max_length=25, blank=False, null=False)
+    port = models.IntegerField(null=True, default=0)
+    status = models.BooleanField(null=False, default=False)
+
+    class Meta:
+        """
+        Meta
+        """
+        db_table = 'work_station'
+        verbose_name = 'Work Station'
+        verbose_name_plural = 'Work Station'
